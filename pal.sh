530ccd4c99230c5554562d3daecaca5ed01c15d1
abokareem <elnwajha.pal@gmail.com>
2019-11-22 22:47:03 +0300

    Update pal.sh

--- pal.sh
+++ pal.sh
@@ -9,11 +9,9 @@ sudo apt update -y
 sudo apt install apache2 -y
 sudo apt-get install mariadb-server mariadb-client -y
 sudo mysql_secure_installation -y
-sudo mysql -u root -p
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt install php7.1 libapache2-mod-php7.1 php7.2-common php7.1-sqlite3 php7.1-curl php7.1-intl php7.1-mbstring php7.1-mcrypt php7.1-xmlrpc php7.1-mysql php7.1-gd php7.1-xml php7.1-cli php7.1-zip
 
 
 echo "Installing Nginx"
@@ -81,12 +79,9 @@ sudo sed -i "s/max_execution_time = .*/max_execution_time = 360/" /etc/php/7.1/a
 sudo sed -i "s/date.timezone = .*/date.timezone = America/Chicago/" /etc/php/7.1/apache2/php.ini
 
 
-echo "Some phpinfo.php tweaks"
-sleep 2;
-sudo systemctl restart apache2.service
-sudo sed -i "s/ = .*/ = <?php phpinfo( ); ?>/" /var/www/html/phpinfo.php
 
-sudo systemctl restart php7.0-fpm.service
+sudo systemctl restart apache2.service
+
 
 echo "Instaling MariaDB"
 sleep 2;
